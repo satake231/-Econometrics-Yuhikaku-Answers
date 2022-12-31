1a79e81a7701a7f405039845fa5f92d3ec0ceb49
satake231 <r1ryoichiros2@gmail.com>
2022-12-31 16:02:24 +0900

    add 2-1.R

--- Ⅰ/Chapter 2/Python/2-1.py
+++ Ⅰ/Chapter 2/Python/2-1.py
@@ -1,15 +1,15 @@
 import numpy as np
 from scipy import stats
 
-array = np.array([12, 15, 9, 17, 5, 8, 10, 12, 6, 3, 10, 11, 20, 6, 8, 10, 13, 10, 11, 4])
+a = np.array([12, 15, 9, 17, 5, 8, 10, 12, 6, 3, 10, 11, 20, 6, 8, 10, 13, 10, 11, 4])
 
-mean = np.mean(array)
+mean = np.mean(a)
 
-var = np.var(array)
+var = np.var(a)
 
-mdn = np.median(array)
+mdn = np.median(a)
 
-count = stats.mode(array, axis=None)
+count = stats.mode(a, axis=None)
 mode = count[0][0]
 
 print(" mean={} \n variance={} \n median={} \n mode={}".format(mean, var, mdn, mode))
---
+++ Ⅰ/Chapter 2/R/2-1.R
@@ -0,0 +1,14 @@
+a <- c(12, 15, 9, 17, 5, 8, 10, 12, 6, 3, 10, 11, 20, 6, 8, 10, 13, 10, 11, 4)
+
+mean <- mean(a)
+
+var <- var(a)
+
+mdn <- median(a)
+
+mode <- mode(a)
+
+paste("mean =", mean)
+paste("variance =", var)
+paste("median =", mdn)
+paste("mode =", mode)
